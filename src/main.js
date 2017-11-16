908184965b7e0b3369710cfdf1b3449a96506705
qiufuli <1760067419@qq.com>
2017-11-16 13:40:38 +0800

    基本样式 路由和Tab 171116

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import fastclick from 'fastclick'
+import '@/common/stylus/index.styl'
+//解决移动端300ms延时的问题 固定写法
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+	router,
   template: '<App/>',
   components: { App }
 })
