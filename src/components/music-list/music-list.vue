6c1edc347997796021cc5f86da31d6f0f03b939b
qiufuli <1760067419@qq.com>
2017-11-30 17:51:51 +0800

    singer-detail2 171130

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,16 +1,27 @@
 <template>
 	<div class="music-list">
-		<div class="back">
+		<div class="back" @click="back">
 			<i class="icon-back"></i>
 		</div>
 		<h1 class="title" v-html="title"></h1>
-		<div class="bg-image" :style="bgStyle" ref="bgStyle">
+		<div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" ref="playBtn" v-show="songs.length>0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
 			<div class="filter" ref="filter"></div>
 		</div>
-		<scroll :data="songs" class="list" ref="list">
+    <div class="bg-layer" ref="layer"></div>
+		<scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
 			<div class="song-list-wrapper">
 				<song-list :songs="songs"></song-list>
 			</div>
+      			<!-- loading 需要有个包裹层-->
+			<div v-show="!songs.length" class="loading-container">
+	        	<loading></loading>
+	    </div>
 		</scroll>
 	</div>
 
@@ -20,6 +31,11 @@
 import Scroll from '@/base/scroll/scroll'
 import SongList from '@/base/song-list/song-list'
 import Loading from '@/base/loading/loading'
+import {prefixStyle} from '@/common/js/dom'
+
+const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform');
+const backdrop = prefixStyle('backdrop-filter');
 export default{
 	components:{
 		Scroll,
@@ -40,14 +56,73 @@ export default{
 			default:''
 		}
 	},
+  data(){
+    return{
+      scrollY:0
+    }
+  },
+  created(){
+    this.probeType = 3;
+    this.listenScroll = true;
+  },
 	mounted(){
-		this.$refs.list.$el.style.top=`${this.$refs.bgStyle.clientHeight}px`
+    this.imageHeight = this.$refs.bgImage.clientHeight;
+    console.log(' this.imageHeight', this.imageHeight)
+    this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT;
+    
+		this.$refs.list.$el.style.top=`${this.imageHeight}px`
 	},
 	computed:{
 		bgStyle(){
 			return `background-image:url(${this.bgImage})`
 		}
-	}
+	},
+  methods:{
+    scroll(pos){
+      this.scrollY = pos.y;
+    },
+    back(){
+      this.$router.back();
+    }
+  },
+  watch:{
+    scrollY(newY){
+      let translateY = Math.max(this.minTranslateY,newY);
+      this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`;
+      let zIndex = 0;
+      let scale = 1;
+      // 高斯模糊
+      let blur = 0;
+    
+      // 滚动到顶上的时候让高度为40px 设置zindex 然后滚下来得时候就恢复
+      if(newY < this.minTranslateY){
+        zIndex = 10;
+        this.$refs.bgImage.style.paddingTop = 0;
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`;  
+        this.$refs.playBtn.style.display="none";      
+      }else{
+        this.$refs.bgImage.style.paddingTop ='70%';
+        this.$refs.bgImage.style.height = 0; 
+        this.$refs.playBtn.style.display='';     
+      }
+   
+      
+      // 下拉得时候图片变大
+      const percent = Math.abs(newY / this.imageHeight);
+       if(newY > 0){
+          scale =1+percent;
+          zIndex = 10
+      }else{
+        blur = Math.min(20*percent,20 )
+      }
+       this.$refs.filter.style[backdrop] = `blur(${blur}px)`;
+       
+      this.$refs.bgImage.style[transform] = `scale(${scale})`;
+      
+      
+      this.$refs.bgImage.style.zIndex = zIndex;
+    }
+  }
 }
 </script>
 
